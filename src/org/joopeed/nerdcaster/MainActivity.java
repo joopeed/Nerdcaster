4c23bc39780dd752c3d531162d3912fec339860c
João Pedro Leôncio <joopeeds@gmail.com>
2014-08-10 21:13:00 -0300

    Player as a service

--- src/org/joopeed/nerdcaster/MainActivity.java
+++ src/org/joopeed/nerdcaster/MainActivity.java
@@ -3,27 +3,36 @@ package org.joopeed.nerdcaster;
 import java.io.IOException;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+        implements NavigationDrawerFragment.NavigationDrawerCallbacks, OnClickListener {
 
     /**
      * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
@@ -36,36 +45,127 @@ public class MainActivity extends ActionBarActivity
     private CharSequence mTitle;
     
     
-    private MediaPlayer mp = new MediaPlayer();
+    
+    
+    final String SUGGESTED_URL = "http://jovemnerd.com.br/podpress_trac/feed/100596/0/nerdcast_426_nerdtour_california_comic_con.mp3";
+    Button mPlayButton;
+    Button mPauseButton;
+    Button mSkipButton;
+    Button mRewindButton;
+    Button mStopButton;
+    Button mEjectButton;
+    
+    //private MediaPlayer mp = new MediaPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
         mTitle = getTitle();
 
         
-	    try {
+        
+        //mPlayButton = (Button) findViewById(R.id.button1);
+        //mPauseButton = (Button) findViewById(R.id.pausebutton);
+       // mSkipButton = (Button) findViewById(R.id.skipbutton);
+        //mRewindButton = (Button) findViewById(R.id.rewindbutton);
+        //mStopButton = (Button) findViewById(R.id.stopbutton);
+        //mEjectButton = (Button) findViewById(R.id.ejectbutton);
+        //mPlayButton.setOnClickListener(this);
+        //mPauseButton.setOnClickListener(this);
+        //mSkipButton.setOnClickListener(this);
+        //mRewindButton.setOnClickListener(this);
+        //mStopButton.setOnClickListener(this);
+        //mEjectButton.setOnClickListener(this);
+        
+	 /*   try {
 	        	if(!mp.isPlaying()) { 
 	            mp.reset();
-	            mp.setDataSource("http://jovemnerd.com.br/podpress_trac/feed/100596/0/nerdcast_426_nerdtour_california_comic_con.mp3");
+	            mp.setDataSource("");
 	            mp.prepare();
 	            mp.start();
 	        	}
 	            
-	
+	        	// 	Use the media button APIs (if available) to register ourselves for media button
+	            // events
+	        	
+	            
 	    } catch (IOException e) {
 	    }
-        
+        */
        
         // Set up the drawer.
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
     }
+    
+    
+    
+    
+    
+    
+    public void onClick(View target) {
+        // Send the correct intent to the MusicService, according to the button that was clicked
+        if (target == mPlayButton)
+            startService(new Intent(MusicService.ACTION_PLAY));
+       
+        
+        
+        
+        
+        
+        /*else if (target == mPauseButton)
+            startService(new Intent(MusicService.ACTION_PAUSE));
+        else if (target == mSkipButton)
+            startService(new Intent(MusicService.ACTION_SKIP));
+        else if (target == mRewindButton)
+            startService(new Intent(MusicService.ACTION_REWIND));
+        else if (target == mStopButton)
+            startService(new Intent(MusicService.ACTION_STOP));
+        else if (target == mEjectButton) {
+            showUrlDialog();
+        }*/
+    }
+    
+    
+    void showUrlDialog() {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+        alertBuilder.setTitle("Manual Input");
+        alertBuilder.setMessage("Enter a URL (must be http://)");
+        final EditText input = new EditText(this);
+        alertBuilder.setView(input);
+        input.setText(SUGGESTED_URL);
+        alertBuilder.setPositiveButton("Play!", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dlg, int whichButton) {
+                // Send an intent with the URL of the song to play. This is expected by
+                // MusicService.
+                Intent i = new Intent(MusicService.ACTION_URL);
+                Uri uri = Uri.parse(input.getText().toString());
+                i.setData(uri);
+                startService(i);
+            }
+        });
+        alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dlg, int whichButton) {}
+        });
+        alertBuilder.show();
+    }
+    
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE:
+            case KeyEvent.KEYCODE_HEADSETHOOK:
+                startService(new Intent(MusicService.ACTION_TOGGLE_PLAYBACK));
+                return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
 
     @Override
     public void onNavigationDrawerItemSelected(int position) {
@@ -78,13 +178,15 @@ public class MainActivity extends ActionBarActivity
 
     public void onSectionAttached(int number) {
         switch (number) {
-            case 1:
+            case 1:	 	
                 mTitle = getString(R.string.title_section1);
                 break;
             case 2:
+            	startService(new Intent(MusicService.ACTION_PLAY));  
                 mTitle = getString(R.string.title_section2);
                 break;
             case 3:
+            	showUrlDialog(); 
                 mTitle = getString(R.string.title_section3);
                 break;
         }
